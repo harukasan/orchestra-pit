cd9ac7879d65f82e242e4dd374637522f1b3abda
Shunsuke MICHII <harukasan@me.com>
2015-06-30 10:36:07 +0900

    Add exec state to execute external command.

--- commands/exec/exec.go
+++ commands/exec/exec.go
@@ -1,6 +1,15 @@
+// Copyright 2015 MICHII Shunsuke. All rights reserved.
+
+/*
+Package exec provides the execution of commands.
+*/
 package exec
 
-import "os/exec"
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
 
 // Cmd represents an external command.
 type Cmd struct {
@@ -12,4 +21,41 @@ type Cmd struct {
 func Command(name string, arg ...string) *Cmd {
 	c := exec.Command(name, arg...)
 	return &Cmd{c}
+}
+
+// Exec is a dummy state which executes the given script when the state is
+// applied.
+//
+// Script specifies the command to execute on that the state is applied.
+//
+// TestScript specifies the command to execute on testing the state. If the
+// command returns an errro when the command exits with non-zero status.
+//
+type Exec struct {
+	Script     string
+	TestScript string
+	Env        []string
+}
+
+// Apply tries to execute the given script.
+func (s *Exec) Apply() error {
+	args := strings.Split(s.Script, " ")
+	c := Command(args[0], args[1:]...)
+	c.Env = append(c.Env, os.Environ()...)
+	c.Env = append(c.Env, s.Env...)
+	return c.Run()
+}
+
+// Test tries to execute the given TestScript, if the TestScript parameter is
+// not empty.
+func (s *Exec) Test() error {
+	if s.TestScript == "" {
+		return nil
+	}
+
+	args := strings.Split(s.TestScript, " ")
+	c := Command(args[0], args[1:]...)
+	c.Env = append(c.Env, os.Environ()...)
+	c.Env = append(c.Env, s.Env...)
+	return c.Run()
 }
---
+++ commands/exec/exec_test.go
@@ -0,0 +1,21 @@
+package exec_test
+
+import (
+	"testing"
+
+	"github.com/harukasan/orchestra-pit/commands/exec"
+)
+
+func TestExec(t *testing.T) {
+	s := &exec.Exec{
+		Script:     "echo apply",
+		TestScript: "echo test",
+	}
+
+	if err := s.Apply(); err != nil {
+		t.Errorf("Apply: %v", err)
+	}
+	if err := s.Test(); err != nil {
+		t.Errorf("Test: %v", err)
+	}
+}
